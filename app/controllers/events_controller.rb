3acdd9b89710c5b102c6f8e16d0c8d046b3c4a44
Sayali Pathak <mudrakalpa@gmail.com>
2024-09-22 00:49:15 +0530

    Create Iterable API Helper and use it to send events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,10 @@
+require 'iterable_helper'
 class EventsController < ApplicationController
   def index
   end
 
   def create
+    response = IterableHelper.new.create_event('xyz@gmail.com', params[:event_type])
     event_type = params[:event_type]
     render json: {type: event_type}
   end
---
+++ lib/iterable_helper.rb
@@ -0,0 +1,43 @@
+require 'faraday'
+
+ITERABLE_BASE_URL = "https://api.iterable.com"
+API_KEY = "mockAPIKey"
+
+class IterableHelper
+  def create_event(email, event_type)
+    conn = create_connection
+
+    response = conn.post('/api/events/track') do |req|
+      req.headers['x-api-key'] = API_KEY
+      req.body = {
+        email: email,
+        eventName: event_type,
+        createdAt: Time.now.to_i
+    }.to_json
+    end
+
+    response
+  end
+  
+  def send_email(email)
+    conn = create_connection
+
+    response = conn.post('/api/email/target') do |req|
+      req.headers['x-api-key'] = API_KEY
+      req.body = {
+        recipientEmail: email
+      }
+    end
+
+    response
+  end
+
+  def create_connection
+    Faraday.new(url: ITERABLE_BASE_URL, headers: {'Content-Type' => 'application/json'}) do |faraday|
+      faraday.request  :url_encoded
+      faraday.response :logger
+      faraday.adapter  Faraday.default_adapter
+    end
+  end
+end
+   
