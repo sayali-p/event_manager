9db0f225a2c7e03252578d65d19b078526ac446f
Sayali Pathak <mudrakalpa@gmail.com>
2024-09-22 17:51:38 +0530

    Use current users email id instead of hardcoded value

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,20 +1,21 @@
 require 'iterable_helper'
 class EventsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
   end
 
   def create
     event_type = params[:event_type]
     iterable = IterableHelper.new
-
     # Create event if it's type is 'A' or 'B'
     if event_type == 'A' or event_type == 'B'
-      response = iterable.create_event('xyz@gmail.com', params[:event_type])
+      response = iterable.create_event(current_user.email, params[:event_type])
     end
 
     # Send email only if event is already created.
     if event_type == 'B' and response.status == 200
-      response = iterable.send_email('xyz@gmail.com')
+      response = iterable.send_email(current_user.email)
     end
 
     if response.status == 200
